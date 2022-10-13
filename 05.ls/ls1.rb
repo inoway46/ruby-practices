bb9a3e157c477568957df0643d8cf36fe80c4689
inoway <inoway@pepabo.com>
2022-10-13 11:43:19 +0900

    fix 変数名、配列生成処理の分岐、ljustの引数

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -1,30 +1,37 @@
-array = Dir.glob("*").sort
+current_dir_files = Dir.glob("*").sort
 
-count = array.size
+files_num = current_dir_files.size
 
-if count%3 != 0
-  num = count/3 + 1
+if files_num % 3 != 0
+  split_length = (files_num / 3) + 1
 else
-  num = count/3
+  split_length = files_num / 3
 end
 
-array2 = array.each_slice(num).to_a
+split_array = current_dir_files.each_slice(split_length).to_a
 
-files = []
-num.times do |i|
-  3.times do |j|
-    if !array2[j][i].nil?
-      files << array2[j][i]
-    else
-      files << ""
+ordered_files = []
+
+if files_num > 3
+  split_length.times do |i|
+    3.times do |j|
+      if !split_array[j][i].nil?
+        ordered_files << split_array[j][i]
+      else
+        ordered_files << ""
+      end
     end
   end
+else
+  ordered_files = current_dir_files
 end
 
-files.each_with_index do |file, i|
-  if i%3 == 2
-    print "#{file.to_s.ljust(10)}\n"
+max_str = ordered_files.map(&:length).max + 2
+
+ordered_files.each_with_index do |file, idx|
+  if idx % 3 == 2
+    print "#{file.to_s.ljust(max_str)}\n"
   else
-    print file.to_s.ljust(10)
+    print "#{file.to_s.ljust(max_str)}"
   end
 end
