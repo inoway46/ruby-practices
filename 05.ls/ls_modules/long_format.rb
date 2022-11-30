ad1ff8284b421bb137647c1d7380833860b46d96
inoway46 <inoueyuya416@gmail.com>
2022-11-30 16:29:29 +0900

    Refactor lint:too_many_linesの修正のため定数を2列で記述

--- 05.ls/ls_modules/long_format.rb
+++ 05.ls/ls_modules/long_format.rb
@@ -9,36 +9,24 @@ module ListSegment
     include FileOption
 
     PERMISSION_PATTERNS = {
-      '0' => '---',
-      '1' => '--x',
-      '2' => '-w-',
-      '3' => '-wx',
-      '4' => 'r--',
-      '5' => 'r-x',
-      '6' => 'rw-',
-      '7' => 'rwx'
+      '0' => '---', '1' => '--x',
+      '2' => '-w-', '3' => '-wx',
+      '4' => 'r--', '5' => 'r-x',
+      '6' => 'rw-', '7' => 'rwx'
     }.freeze
 
     UID_PERMISSION_PATTERNS = {
-      '0' => '--S',
-      '1' => '--s',
-      '2' => '-wS',
-      '3' => '-ws',
-      '4' => 'r-S',
-      '5' => 'r-s',
-      '6' => 'rwS',
-      '7' => 'rws'
+      '0' => '--S', '1' => '--s',
+      '2' => '-wS', '3' => '-ws',
+      '4' => 'r-S', '5' => 'r-s',
+      '6' => 'rwS', '7' => 'rws'
     }.freeze
 
     STICKY_PERMISSION_PATTERNS = {
-      '0' => '--T',
-      '1' => '--t',
-      '2' => '-wT',
-      '3' => '-wt',
-      '4' => 'r-T',
-      '5' => 'r-t',
-      '6' => 'rwT',
-      '7' => 'rwt'
+      '0' => '--T', '1' => '--t',
+      '2' => '-wT', '3' => '-wt',
+      '4' => 'r-T', '5' => 'r-t',
+      '6' => 'rwT', '7' => 'rwt'
     }.freeze
 
     def initialize(options = {})
